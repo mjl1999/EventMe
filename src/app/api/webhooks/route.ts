34c0df585bb83f0bba810fadcfd2c1a9aa71d828
Matthew Lee <matthewjameslee@hotmail.co.uk>
2025-06-03 01:04:28 +0100

    tidied up files

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -6,14 +6,7 @@ export async function POST(req: NextRequest) {
   try {
     const evt = await verifyWebhook(req);
 
-    // Do something with payload
-    // For this guide, log payload to console
-    // const { id } = evt.data;
-    // const eventType = evt.type;
-    // console.log(
-    //   `Received webhook with ID ${id} and event type of ${eventType}`
-    // );
-
+    
     if (evt.type === "user.created") {
       const { id, email_addresses, first_name, last_name, username } = evt.data;
       try {
